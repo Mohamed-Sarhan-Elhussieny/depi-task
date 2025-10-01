d63d7a0b1f55d7668c6c47928f81c2eadc6746b0
Mohamed Sarhan <your_email@example.com>
2025-10-01 23:31:05 +0300

    nginx lb

---
+++ k8s /t6/index.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node App 2!");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
